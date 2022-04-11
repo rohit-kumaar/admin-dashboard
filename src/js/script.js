c04d2b4362f30a6c066f860f53f69db2ede49eba
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-04-11 18:56:12 +0530

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+/* This is a function that is listening for the DOM to be ready. */
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  console.log("script JS file");
+};
+
+// querySelectorAll function
+function qsAll(selector, parent = document) {
+  return [...parent.querySelectorAll(selector)];
+}
+
+// querySelector function
+function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
