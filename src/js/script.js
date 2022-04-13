e5e980468913afdddf7412af50187537f58f3842
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-04-13 17:46:34 +0530

    Canvas Chart

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("readystatechange", (event) => {
 });
 
 const initApp = () => {
-  console.log("script JS file");
+  chart();
 };
 
 // querySelectorAll function
@@ -17,4 +17,50 @@ function qsAll(selector, parent = document) {
 // querySelector function
 function qs(selector, parent = document) {
   return parent.querySelector(selector);
+}
+
+function chart() {
+  const chart = qs("#chart").getContext("2d");
+  // Create a new chart instance
+  new Chart(chart, {
+    type: "line",
+    data: {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+      ],
+      datasets: [
+        {
+          label: "BTC",
+          data: [
+            12372, 45283, 64274, 94237, 31274, 23670, 21548, 34862, 23573,
+            64529,
+          ],
+          borderColor: "red",
+          borderWidth: 2,
+        },
+        {
+          label: "ETH",
+          data: [
+            73607, 94004, 40963, 79240, 89111, 81917, 17009, 44647, 35110,
+            40619,
+          ],
+          borderColor: "blue",
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 }
