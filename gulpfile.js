56159856e0f112da73d5fe487794efb08acc94ce
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2023-09-15 17:25:02 +0530

    update package

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,18 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var sourcemaps = require("gulp-sourcemaps");
-// var autoprefixer = require('gulp-autoprefixer');
-var csso = require("gulp-csso");
-var concat = require("gulp-concat");
-var minify = require("gulp-minify");
-var cleanCss = require("gulp-clean-css");
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const minify = require("gulp-minify");
+const cleanCss = require("gulp-clean-css");
 
 // Development Tasks
 gulp.task("sass", function () {
-  return (
-    gulp
-      .src("src/scss/**/*.scss") // Gets all files ending with .scss in src/scss and children dirs
-      .pipe(sourcemaps.init())
-      .pipe(sass().on("error", sass.logError)) // Passes it through a gulp-sass, log errors to console
-      // .pipe(autoprefixer({
-      //   cascade: false
-      // }))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest("src/css"))
-  ); // Outputs it in the css folder
+  return gulp
+    .src("src/scss/**/*.scss") // Gets all files ending with .scss in src/scss and children dirs
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("src/css")); // Outputs it in the css folder
 });
 
 // Watchers
@@ -35,16 +28,13 @@ gulp.task("minifycss", function () {
       // Compile SASS files
       .pipe(
         sass({
-          outputStyle: "nested",
           precision: 10,
           includePaths: ["."],
           onError: console.error.bind(console, "Sass error:"),
         })
       )
       .pipe(concat("bundle.min.css"))
-      // Auto-prefix css styles for cross browser compatibility
-      // .pipe(autoprefixer())
-      // Minify the file
+
       .pipe(cleanCss())
       // Output
       .pipe(gulp.dest("src/css"))
@@ -55,7 +45,7 @@ gulp.task("minifycss", function () {
 gulp.task("minifyjs", function () {
   return (
     gulp
-      .src(["src/js/browser-class.js", "src/js/global.js"])
+      .src(["src/js/index.js"])
       // Minify the file
       .pipe(concat("bundle.min.js"))
       .pipe(
